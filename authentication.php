ee47c7eaef20dc685391b15f272d26408175d04c
Justin <justinquach11214@gmail.com>
2023-03-10 18:01:31 -0500

    added backend
    -moved php files out of folder
    -changed index code to verify user accounts

--- authentication.php
+++ authentication.php
@@ -2,8 +2,7 @@
     include('connection.php');  
     $username = $_POST['user'];  
     $password = $_POST['pass'];  
-      
-        //to prevent from mysqli injection  
+       
         $username = stripcslashes($username);  
         $password = stripcslashes($password);  
         $username = mysqli_real_escape_string($con, $username);  
